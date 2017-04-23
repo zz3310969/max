1db9dcaafb036f63411a1fc6f76d19db3d5acfe0
zz3310969 <zz3310969@163.com>
2017-04-23 20:25:06 +0800

    update

---
+++ springmvcdemo/src/main/java/com/max/DemotionCommand.java
@@ -0,0 +1,38 @@
+package com.max;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Hc on 17/4/23.
+ */
+public class DemotionCommand extends HystrixCommand<String> {
+    private final String name;
+    public DemotionCommand(String name) {
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("HelloWorldGroup"))
+                /* 配置依赖超时时间,500毫秒*/
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionIsolationThreadTimeoutInMilliseconds(500)));
+        this.name = name;
+    }
+    @Override
+    protected String getFallback() {
+        return "exeucute Falled";
+    }
+    @Override
+    protected String run() throws Exception {
+        //sleep 1 秒,调用会超时
+        TimeUnit.MILLISECONDS.sleep(1000);
+        return "Hello " + name +" thread:" + Thread.currentThread().getName();
+    }
+
+    public static void main(String[] args) throws Exception{
+        DemotionCommand command = new DemotionCommand("test-Fallback");
+        String result = command.execute();
+
+        System.out.println(result);
+
+    }
+}
